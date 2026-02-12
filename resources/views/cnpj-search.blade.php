347d777edbfd59beeb41ac6c33c47e20668a26ad
Tiago Gonçalves <tiago@tiremoto.com.br>
2026-02-12 14:36:16 -0300

    Correção de Build

--- resources/views/cnpj-search.blade.php
+++ resources/views/cnpj-search.blade.php
@@ -5,13 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Consulta de CNPJ - Sistema de Busca</title>
     
-    <!-- Tailwind CSS -->
-    <script src="https://cdn.tailwindcss.com"></script>
-    
     <!-- Fontes -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     <style>
         body {
